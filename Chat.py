bc0966f2a0172f2fa02bffeb1b4f0f57ce577c81
Sujith Ramprasad <47356838+Sujithrt@users.noreply.github.com>
2024-02-06 21:28:46 -0700

    Implemented Chat history and LLM response streaming

--- Chat.py
+++ Chat.py
@@ -1,33 +1,23 @@
 import streamlit as st
 from langchain.schema import HumanMessage, SystemMessage, AIMessage
-from langchain_community.callbacks import OpenAICallbackHandler, get_openai_callback
+from langchain.callbacks.base import BaseCallbackHandler
 
 from utils.data_loader import populate_vector_store, scrape_link
 from utils.initialize_vector_store import initialize_vector_store
-from utils.create_agent_executor import create_agent_executor
+from utils.create_agent_executor_1 import create_agent_executor
 
 
-def update_usage(cb: OpenAICallbackHandler) -> None:
-    callback_properties = [
-        "total_tokens",
-        "prompt_tokens",
-        "completion_tokens",
-        "total_cost",
-    ]
-    print('Callback', cb)
-    for prop in callback_properties:
-        value = getattr(cb, prop, 0)
-        st.session_state.usage[prop] += value
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
 
 
 def main():
-    if "usage" not in st.session_state:
-        st.session_state.usage = {
-            "total_tokens": 0,
-            "prompt_tokens": 0,
-            "completion_tokens": 0,
-            "total_cost": 0.0,
-        }
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
@@ -36,10 +26,7 @@ def main():
     st.set_page_config(page_title="Healthcare Chatbot", page_icon=":robot_face:")
     st.header('Healthcare Chatbot')
     with st.sidebar:
-        st.header("API Usage")
-        if st.session_state["usage"]:
-            st.metric("Total Tokens", st.session_state["usage"]["total_tokens"])
-            st.metric("Total Costs in $", st.session_state["usage"]["total_cost"])
+        st.header("Upload Section")
         with st.container(border=True):
             st.markdown("### Upload Files")
             uploaded_files = st.file_uploader("Upload a file",
@@ -82,16 +69,14 @@ def main():
         with st.chat_message(role):
             st.markdown(content)
 
-    agent_executor = create_agent_executor(astra_vector_store, st.secrets['OPENAI_API_KEY'])
-
     if user_input := st.chat_input("Ask me anything"):
         st.session_state.messages.append(HumanMessage(content=user_input))
         st.chat_message("user").markdown(user_input)
-        with get_openai_callback() as cb:
-            response = agent_executor.invoke({"input": user_input})
-            update_usage(cb)
-            st.chat_message("assistant").markdown(response['output'])
-            st.session_state.messages.append(AIMessage(content=response['output']))
+        with st.chat_message("assistant"):
+            stream_handler = StreamHandler(st.empty())
+            agent_executor = create_agent_executor(astra_vector_store, st.secrets['OPENAI_API_KEY'], stream_handler)
+            response = agent_executor.invoke({"input": user_input, "chat_history": st.session_state.messages})
+        st.session_state.messages.append(AIMessage(content=response['output']))
 
 
 if __name__ == "__main__":
